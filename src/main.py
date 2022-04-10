2bc68ae511a866f93ac4e24ebd0deac32ced18bb
Dhravya <dhravyashah@gmail.com>
2022-04-10 21:39:56 +0530

    made terminal look better, added docstrings, made it easier to use + cleanup function

--- src/main.py
+++ src/main.py
@@ -24,7 +24,7 @@ class DeepDubber:
 
         print(f"[bold blue]Using Video path: [/bold blue] {self.video_path}")
 
-    def _translate_subtitles(
+    def __translate_subtitles(
         self, subtitles: List[Tuple[str, Tuple[float, float]]], *, language: str
     ) -> List[Tuple[str, Tuple[float, float]]]:
         """Translates the subtitles into the target language."""
@@ -46,9 +46,13 @@ class DeepDubber:
             translated_words, subtitles
         )
 
+        # Removes previous line from terminal
+        print("\033[F\033[K", end="")
+
         print(
             f"✅ [green]Translated the subtitles into [red]{LANGUAGES[language]}[/red]"
         )
+
         return translated_subtitles
 
     def __create_translated_subtitles(
@@ -61,11 +65,13 @@ class DeepDubber:
 
         for word, (start, end) in subtitles:
             translated_subtitles.append((translated_words.pop(0), (start, end)))
-        print(translated_subtitles)
         return translated_subtitles
 
-    def _convert_to_speech(
-        self, subtitles: List[Tuple[str, Tuple[float, float]]], initial_lang: str = "en"
+    def __convert_to_speech(
+        self,
+        subtitles: List[Tuple[str, Tuple[float, float]]],
+        initial_lang: str = "en",
+        output_file="output.mp4",
     ) -> None:
         """Converts the subtitles into speech and saves them to the disk."""
         print("⚡ Converting the subtitles into speech")
@@ -76,17 +82,18 @@ class DeepDubber:
             tts = gTTS(word, lang=initial_lang)
             audio_arr.append(tts)
 
+        print("\033[F\033[K", end="")
         print("✅ [green]Converted the subtitles into speech.[/green]")
 
-        os.makedirs("audio.temp", exist_ok=True)
+        os.makedirs("temp", exist_ok=True)
         # temperory folder to save the audio
-        audio_folder = os.path.join(os.getcwd(), "audio.temp")
+        audio_folder = os.path.join(os.getcwd(), "temp")
 
         for i, audio in enumerate(audio_arr):
             audio.save(os.path.join(audio_folder, f"{i}.mp3"))
 
         print("⚡ Merging the audio files")
-        audio_file = os.path.join(os.getcwd(), "final.mp4")
+        audio_file = os.path.join(os.getcwd(), output_file)
 
         # Remove sound from original video
         final_vid = self.video.copy()
@@ -103,11 +110,12 @@ class DeepDubber:
             )
 
         final_vid.audio = mp.CompositeAudioClip(mp_audio_arr)
+        print("\033[F\033[K", end="")
         print("✅ [green]Merged the audio files.[/green]")
 
         print("⚡ Saving the audio file")
         final_vid.write_videofile(audio_file, audio_codec="aac")
-        print("✅ [green]Saved the audio file.[/green]")
+        print(f"✅ [green]Saved the final file.[/green]\n> [blue]{output_file}[/blue]")
 
     async def _get_subtitles(self) -> List[Tuple[float, str]]:
         """Runs the video path through Deepgram and returns the subtitles as a list of tuples."""
@@ -122,7 +130,7 @@ class DeepDubber:
 
         print("⚡ Fetching the Video Transcripts from [red]Deepgram[/red]")
         response = await dg_client.transcription.prerecorded(source, options)
-
+        print("\033[F\033[K", end="")
         print(
             "✅ [green]Deepgram has sent back the subtitles.[/green] [blue]I will now process them and render them on the video.[/blue]"
         )
@@ -136,14 +144,54 @@ class DeepDubber:
 
         return subtitles
 
-    def run(self, *, language: str, initial_lang: str = "en") -> None:
+    @staticmethod
+    def cleanup() -> None:
+        """Gets rid of all unnecessary files"""
+        print("⚡ Cleaning up the files")
+        try:
+            # Delete temp folder
+            for file in os.listdir("temp"):
+                os.remove(os.path.join("temp", file))
+            os.rmdir("temp")
+
+            print("✅ [green]Deleted the temp folder.[/green]")
+        except FileNotFoundError:
+            print("✅ [red]No files to clean up.[/red]")
+
+    def run(
+        self,
+        output_file="output.mp4",
+        *,
+        language: str,
+        initial_lang: str = "en",
+        cleanup: bool = True,
+    ) -> None:
+        """Gets the subtitles, Converts them into speech and dubs the video.
+
+        Args:
+            output_file (str, optional): The name of the output file. Defaults to "output.mp4".
+            language (str): The language to translate the subtitles into.
+            initial_lang (str, optional): The language of the subtitles. Defaults to "en".
+            cleanup (bool, optional): Whether to delete the temp folder. Defaults to True.
+
+        Language Codes:
+            https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+
+        Example:
+            >>> dubber = DeepDubber("video.mp4")
+            >>> dubber.run("output.mp4", language="es")
+        """
         subtitles = asyncio.get_event_loop().run_until_complete(self._get_subtitles())
-        subtitles = self._translate_subtitles(subtitles, language=language)
-        self._convert_to_speech(subtitles, initial_lang=initial_lang)
+        subtitles = self.__translate_subtitles(subtitles, language=language)
+        self.__convert_to_speech(
+            subtitles, initial_lang=initial_lang, output_file=output_file
+        )
+
+        DeepDubber.cleanup() if cleanup else None
 
         print("⚡ Thanks for using DeepDubber! Have a nice day!")
 
 
 if __name__ == "__main__":
     dubber = DeepDubber(video_path="./demo.mp4")
-    dubber.run(language="fr")
+    dubber.run(language="fr", cleanup=False)
